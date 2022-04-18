529cf23642a83f565ee0471f170b414b97e34bc7
rawkry <sudeeppaudel14@gmail.com>
2022-04-18 19:37:33 +0545

    feedbackapp

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './App.css';
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+import Header from './components/Header';
+import FeedbackData from './data/FeedbackData';
+
+
+function App() {
+
+  const [feedback, setFeedback] = useState(FeedbackData) 
+
+
+    const deleteFeedback = (id) => {
+      if (window.confirm('Are you sure you want to delete?')) {
+      
+        setFeedback(feedback.filter((item) => item.id !== id))
+      
+    }
+  }
+
+
+
+  
+  return (
+    <div className="App">
+      <Header />
+      <div className='container'>
+        <FeedbackStats feedback={feedback} />
+       <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+      
+      
+
+    </div>
+  );
+}
+
+export default App;
